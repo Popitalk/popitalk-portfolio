4115f9244ceca2671850ee64526841f1dda6aa58
Andrew Jang <dhj9817@gmail.com>
2020-09-17 07:54:20 +0900

    Updated team description and deleted some unecessary files

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -33,15 +33,13 @@ export default class About extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
                         <h2 className="colorlib-heading">Our Team</h2>
                     </div>
-                    </div>
-                    <div className="row row-pt-md">
                     <div className="col-md-4 text-center animate-box">
                         <div className="services color-1">
                             <img src={andrew} alt="" />
-                        <div className="desc">
-                            <h3>Andrew</h3>
-                            <p>placeholder: I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
-                        </div>
+                            <div className="desc">
+                                <h3>Andrew</h3>
+                                <p>In charge UI/UX but more importantly maintaining Team Popitalk to have only the brightest people and making sure that we are heading towards the right direction.</p>
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-4 text-center animate-box">
@@ -49,16 +47,16 @@ export default class About extends Component {
                             <img src={emily} alt="" />
                             <div className="desc">
                                 <h3>Emily</h3>
-                                <p>placeholder: As coming from the CS background, I have good grasp over fundamental concepts of DSA</p>
+                                <p>Since graduating with an MS in Computer Science, I’ve helped build numerous startup companies as a software developer, video editor, theater technician, and writer.</p>
                             </div>
                         </div>
                     </div>
                     <div className="col-md-4 text-center animate-box">
                         <div className="services color-2">
-                            <img src={michael} alt="" />
+                            <img src={"https://i.ibb.co/qyXkX3r/unnamed.png"} alt="" />
                             <div className="desc">
-                                <h3>Michael</h3>
-                                <p>placeholder: I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                                <h3>Sulaiman</h3>
+                                <p>I've developed web apps using react, redux and related technologies on the frontend; nodejs, express and SQL databases on the backend. Experience with devops.</p>
                             </div>
                         </div>
                     </div>
@@ -67,15 +65,15 @@ export default class About extends Component {
                             <img src={simon} alt="" />
                             <div className="desc">
                                 <h3>Simon</h3>
-                                <p>placeholder: I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                                <p>I am an avid web developer, with most of my experience in the front-end. I love creating, learning and I constantly reach for new heights.</p>
                             </div>
                         </div>
                     </div>
                     <div className="col-md-4 text-center animate-box">
                         <div className="services color-5">
-                            <img src={"https://i.ibb.co/qyXkX3r/unnamed.png"} alt="" />
+                            <img src={michael} alt="" />
                             <div className="desc">
-                                <h3>Sulaiman</h3>
+                                <h3>Michael</h3>
                                 <p>placeholder: I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
                             </div>
                         </div>
